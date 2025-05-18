44cbe273dcf7166e709dfb6ec4e37b105d7115e4
Suryanshu Mittal <100358189+Alexander-Lucifer@users.noreply.github.com>
2025-05-18 14:20:59 +0530

    SEO-2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -46,5 +46,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 0.4,
     },
+    {
+      url: `${baseUrl}/careers`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/careers/apply`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    
   ];
 } 
